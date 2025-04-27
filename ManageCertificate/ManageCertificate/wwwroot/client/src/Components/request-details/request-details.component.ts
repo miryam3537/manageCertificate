54109d60392a67309a6cca01a516a024b13d8b8d
215557299 <38215557299@mby.co.il>
2025-04-27 14:57:09 +0300

    show all the items

--- ManageCertificate/ManageCertificate/wwwroot/client/src/Components/request-details/request-details.component.ts
+++ ManageCertificate/ManageCertificate/wwwroot/client/src/Components/request-details/request-details.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
+import{RequestDetailsService} from '../../Services/request-details.service';
+import { Requestes } from '../../Models/Requestes';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MatTableDataSource } from '@angular/material/table';
 export interface PeriodicElement {
   name: string;
   position: number;
@@ -9,27 +13,31 @@ export interface PeriodicElement {
 
 @Component({
   selector: 'app-request-details',
-  imports: [MatTableModule],
+  imports: [MatTableModule,HttpClientModule ],
   templateUrl: './request-details.component.html',
-  styleUrl: './request-details.component.css',
- 
+  styleUrls:['./request-details.component.css'] ,
+ providers: [RequestDetailsService]
 })
 export class RequestDetailsComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = [];
+  constructor(private requestDetailsService:RequestDetailsService) { }
+  requestDetails!: Requestes;
+  displayedColumns: string[] = ['certificateTypeNavigation', 'requestAmaunt','Unused balance','supplyAmaunt','comment'];
+  dataSource = new MatTableDataSource<any>([]); // אתחול למערך ריק
+  ngOnInit() {
+    const requestId = 93;
+    this.requestDetailsService.get(requestId).subscribe(
+     (data: Requestes) => {
+        this.requestDetails = data;
+        this.dataSource.data = this.requestDetails.certificates || []; // עדכון ה-dataSource עם הנתונים שהתקבלו
+        console.log(this.requestDetails);
+      },
+     (error) => {
+        console.error('Error fetching request details:', error);
+      }
+    );
+}
 }
 
 
-// const ELEMENT_DATA: PeriodicElement[] = [
-//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-//   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-//   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-//   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-//   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-//   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-// 
+
 
