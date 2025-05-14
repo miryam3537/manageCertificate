7a4fde058a055e044c1dc064d33e0607395c032f
215557299 <38215557299@mby.co.il>
2025-05-14 15:42:07 +0300

    marge

--- ManageCertificate/bl/RequestBL.cs
+++ ManageCertificate/bl/RequestBL.cs
@@ -18,7 +18,7 @@ namespace BL
         IRequestDAl RequestDAl;
         ICertificateDAL CertificateDAL;
         ILogger<RequestBL> logger;
-        public RequestBL(IRequestDAl RequestDAl, ICertificateDAL CertificateDAL,IMapper mapper,ILogger<RequestBL> logger)
+        public RequestBL(IRequestDAl RequestDAl, ICertificateDAL CertificateDAL, IMapper mapper, ILogger<RequestBL> logger)
         {
             this.mapper = mapper;
             this.CertificateDAL = CertificateDAL;
@@ -29,25 +29,25 @@ namespace BL
         {
             return RequestDAl.GetAllRequest();
         }
-        //public async Task<RequestDTO> Get(int id)
-        //{
-        //    Request request = await RequestDAl.Get(id);
-        //    List<Certificate> Allcertificates = (await CertificateDAL.Get(request.Council.Id)).ToList();
-        //    RequestDTO requestDTO = mapper.Map<RequestDTO>(request);
-        //    //requestByIdDto = requestByIdDto with { AllCertificates = certificatesDTO };
-        //    CaldulateSupplayAmountForAllCertificate(Allcertificates, requestDTO.Certificates);
-        //    return requestDTO;
-        //}
-       //public void CaldulateSupplayAmountForAllCertificate(List<Certificate> Allcertificates, IEnumerable<CertificateDTO> certificates)
-       // {
-       //     foreach (CertificateDTO cert in certificates )
-       //     {
-       //         foreach (Certificate allcert in Allcertificates)
-       //         {
-       //             if (cert.CertificateTypeNavigation?.Id == allcert.CertificateType)
-       //                 cert.Used += allcert.SupplyAmaunt;
-       //         }
-       //     }
+        public async Task<RequestDTO> Get(int id)
+        {
+            Request request = await RequestDAl.Get(id);
+            List<Certificate> Allcertificates = (await CertificateDAL.Get(request.Council.Id)).ToList();
+            RequestDTO requestDTO = mapper.Map<RequestDTO>(request);
+            //requestByIdDto = requestByIdDto with { AllCertificates = certificatesDTO };
+            CaldulateSupplayAmountForAllCertificate(Allcertificates, requestDTO.Certificates);
+            return requestDTO;
+        }
+        public void CaldulateSupplayAmountForAllCertificate(List<Certificate> Allcertificates, IEnumerable<CertificateDTO> certificates)
+        {
+            foreach (CertificateDTO cert in certificates)
+            {
+                foreach (Certificate allcert in Allcertificates)
+                {
+                    if (cert.CertificateTypeNavigation?.Id == allcert.CertificateType)
+                        cert.Used += allcert.SupplyAmaunt;
+                }
+            }
 
         }
         public async Task<bool> PutRequestStatus(int id, int previousStatusId, RequestDTO PutRequest)
@@ -58,9 +58,9 @@ namespace BL
 
             if (request.RequestStatus == previousStatusId)
             {
- 
+
                 Request upDateRequest = mapper.Map<DTO.RequestDTO, Request>(PutRequest);
-                await RequestDAl.PutRequestStatus(id,upDateRequest);
+                await RequestDAl.PutRequestStatus(id, upDateRequest);
                 return true; // עדכון הצליח
             }
             else
@@ -73,3 +73,4 @@ namespace BL
 
     }
 }
+
